294b9a99c204029a3a5c11d9afee249780a42606
abhishekpandit <abhishekpandit1212@gmail.com>
2025-04-20 12:37:35 +0530

    fix/cors-origin-*

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ const PORT = process.env.PORT || 4000;
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true,
     },
 });
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
 }));
